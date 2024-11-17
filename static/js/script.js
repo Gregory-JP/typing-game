07bebf5e2caf8665ff680529862e9a236afe0654
Gregory-JP <pitthangregory@gmail.com>
2024-11-17 01:20:32 -0300

    🛠 fix: url fixed

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,7 @@ let typedText = "";
 // Função para chamar a API
 async function fetchStory(query) {
   try {
-    const response = await fetch("http://127.0.0.1:5000/qna", {
+    const response = await fetch("https://typing-game-d0jz.onrender.com/qna", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
