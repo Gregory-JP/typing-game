394e7ea7dbe3332e6a62bfb39b23a6c3b2e1a46a
Gregory-JP <pitthangregory@gmail.com>
2024-11-16 22:40:18 -0300

    🛠 fix: added port config

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from routes.qna_route import qna_function
 from flask import Flask, render_template
+from dotenv import load_dotenv
 from flask_cors import CORS
+import os
 
+load_dotenv()
+
+PORT = os.getenv('PORT')
 
 app = Flask(__name__)
 
@@ -14,3 +19,7 @@ def qna():
 @app.route('/')
 def home():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    port = int(PORT) if PORT else 5000
+    app.run(host='0.0.0.0', port=port)
